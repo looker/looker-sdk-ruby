54f2fbf21fc729d6d715f26391e08cce574c2739
John Bandhauer <jband@looker.com>
2016-04-28 17:39:57 -0700

    add scripts from customer authentication switchover

---
+++ examples/create_credentials_email_for_users.rb
@@ -0,0 +1,19 @@
+require './sdk_setup'
+
+$stdin.each_line do |line|
+  line.chomp!
+
+  id, email = line.split(',', 2).map(&:strip)
+
+  begin
+    user = sdk.user(id)
+    if user.credentials_email
+      puts "Error: User with id '#{id}' Already has credentials_email"
+    else
+      sdk.create_user_credentials_email(id, {:email => email})
+      puts "Success: Created credentials_email for User with id '#{id}' and email '#{email}'"
+    end
+  rescue LookerSDK::NotFound
+    puts "Error: User with id '#{id}' Not found"
+  end
+end
---
+++ examples/refresh_user_notification_addresses.rb
@@ -0,0 +1,10 @@
+require './sdk_setup'
+
+sdk.all_users(:fields => 'id,email').each do |user|
+  new_user = sdk.update_user(user.id, {})
+  if user.email == new_user.email
+    puts "No Change for #{user.id}"
+  else
+    puts "Refreshed #{user.id}. Old email '#{user.email}'. Refreshed email: '#{new_user.email}'."
+  end
+end
