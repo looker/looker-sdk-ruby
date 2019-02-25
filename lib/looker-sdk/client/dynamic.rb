91d1c98a80957647c0c11e05530f89d0e073b53f
Danny Thorpe <danny@looker.com>
2019-02-25 13:34:51 -0800

    Add diagnostic messages when loading swagger.json fails (#83)
    * Add diagnostic messages when loading swagger.json fails

--- lib/looker-sdk/client/dynamic.rb
+++ lib/looker-sdk/client/dynamic.rb
@@ -49,8 +49,27 @@ module LookerSDK
       def load_swagger
         # We only need the swagger if we are going to be building our own 'operations' hash
         return if shared_swagger && @@sharable_operations[api_endpoint]
-        # Try to load w/o authenticating. Else, authenticate and try again.
-        @swagger ||= without_authentication {try_load_swagger} || try_load_swagger
+        # First, try to load swagger.json w/o authenticating
+        @swagger ||= without_authentication { try_load_swagger }
+
+        unless @swagger
+          # capture the bits we may need later, avoiding potential buffer reuse in last_response between requests
+          response_wo_auth_status = last_response&.status
+          response_wo_auth_data = last_response&.data
+
+          # try again, this time with authentication
+          @swagger = try_load_swagger
+        end
+
+        # in unit tests, @swagger may be nil and last_response nil because no HTTP request was made
+        if @swagger.nil? && (response_wo_auth_status || last_response)
+          msg = "Load of swagger.json failed. "
+          msg << "Without authentication HTTP response status: (#{response_wo_auth_status}) data: #{response_wo_auth_data}. " if response_wo_auth_status
+          msg << "WITH authentication HTTP response status: (#{last_response.status}) data: #{last_response.data}" if last_response
+          looker_warn(msg)
+        end
+
+        @swagger
       end
 
       def operations
